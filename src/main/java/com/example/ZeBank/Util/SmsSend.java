651b7ebd5e57cc7dd7100f4f03860ac2a85bf9e2
Mert <zeybekmertanil@gmail.com>
2024-07-16 23:53:24 +0300

    Sms Gönderme işlemi için util yazıldı

---
+++ src/main/java/com/example/ZeBank/Util/SmsSend.java
@@ -0,0 +1,29 @@
+package com.example.ZeBank.Util;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmsSend {
+    @Value("${twilio.account.sid}")
+    private static String ACCOUNT_SID;
+
+    @Value("${twilio.auth.token}")
+    private static  String AUTH_TOKEN;
+
+    @Value("${twilio.phone.number}")
+    private static String TWILIO_PHONE_NUMBER;
+
+    public static void sendTokenSms(String loanStatusMessage) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        String toPhoneNumber = "+905393909280";
+        Message message = Message.creator(
+                new PhoneNumber(toPhoneNumber),
+                new PhoneNumber(TWILIO_PHONE_NUMBER),
+                loanStatusMessage
+        ).create();
+    }
+}
