2ccfb54f67cd0dc7dfe9a289bdbbe13db23bddfd
Mert <zeybekmertanil@gmail.com>
2024-07-16 23:53:54 +0300

    ComponentScan düzenlendi

--- src/main/java/com/example/ZeBank/ZeBankApplication.java
+++ src/main/java/com/example/ZeBank/ZeBankApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.example.ZeBank.Controller","com.example.ZeBank.Util.PasswordUtil",  "com.example.ZeBank.Service", "com.example.ZeBank.Config","com.example.ZeBank.FilterLayer","com.example.ZeBank.Producer",
-        "com.example.ZeBank.Consumer","com.example.ZeBank.Email","com.example.ZeBank.MailConfig"})
+        "com.example.ZeBank.Consumer","com.example.ZeBank.Email","com.example.ZeBank.MailConfig","com.example.ZeBank.Util.SmsSend"})
 
 public class ZeBankApplication {
 
