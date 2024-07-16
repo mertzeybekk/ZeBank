8d71de1952fbeac474b3a9e9f84ce5d8e6fbc4da
Mert <zeybekmertanil@gmail.com>
2024-07-16 23:31:37 +0300

    Hesap İşlemlerinde ssm gönderimi yapıldı

--- src/main/java/com/example/ZeBank/Service/AccountServiceImpl.java
+++ src/main/java/com/example/ZeBank/Service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.ZeBank.Service;
 import com.example.ZeBank.CustomException.ResourceNotFoundException;
 import com.example.ZeBank.Dto.Request.AccountRequestDto;
 import com.example.ZeBank.Dto.Response.AccountResponseDto;
+import com.example.ZeBank.Email.EmailService;
 import com.example.ZeBank.EntityLayer.Account;
 import com.example.ZeBank.EntityLayer.Customer;
 import com.example.ZeBank.EntityLayer.Enum.AccountStatus;
@@ -10,6 +11,7 @@ import com.example.ZeBank.EntityLayer.Enum.AccountType;
 import com.example.ZeBank.MapperLayer.AccountMapper;
 import com.example.ZeBank.RepositoryLayer.AccountRepository;
 import com.example.ZeBank.RepositoryLayer.CustomerRepository;
+import com.example.ZeBank.Util.SmsSend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,12 +26,17 @@ import java.util.stream.Collectors;
 public class AccountServiceImpl extends GenericServiceImpl<Account, AccountRequestDto, AccountResponseDto> implements AccountService {
     private final AccountRepository accountRepository;
     private final CustomerRepository customerRepository;
+    private EmailService emailService;
+    private SmsTokenService smsTokenService;
     private static final Logger logger = LoggerFactory.getLogger(AccountServiceImpl.class);
 
-    public AccountServiceImpl(AccountRepository accountRepository, CustomerRepository customerRepository) {
+    public AccountServiceImpl(AccountRepository accountRepository, CustomerRepository customerRepository,
+                              EmailService emailService, SmsTokenService smsTokenService) {
         super(accountRepository);
         this.accountRepository = accountRepository;
         this.customerRepository = customerRepository;
+        this.emailService = emailService;
+        this.smsTokenService = smsTokenService;
     }
 
     @Override
@@ -72,6 +79,19 @@ public class AccountServiceImpl extends GenericServiceImpl<Account, AccountReque
         account.setCustomer(customer);
 
         Account savedAccount = accountRepository.save(account);
+        String message = String.format("""
+                        Hesap Numarası: %s,
+                        Hesap Durumu %d,
+                        Hesap Tipi: %s,
+                        Hesap Açılış Tarihi %s,               
+                        """,
+                savedAccount.getAccountNumber(),
+                savedAccount.getAccountStatus(),
+                savedAccount.getAccountType(),
+                savedAccount.getAccountOpeningDate()
+        );
+        SmsSend.sendTokenSms(message);
+
         logger.info("Account saved: {}", savedAccount);
         return AccountMapper.convertToAccountResponseDto(savedAccount, AccountType.valueOf(requestDto.getAccountType()));
     }
